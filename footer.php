fc3b2e25a7aaaac1feaeded71877a4c4f6574f56
Philip Zak <pz@myMBP.local>
2015-07-14 18:00:06 -0400

    removed footer content

--- footer.php
+++ footer.php
@@ -11,13 +11,7 @@
 
 	</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'portfolio' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'portfolio' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'portfolio' ), 'portfolio', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer id="colophon" class="site-footer" role="contentinfo"></footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
