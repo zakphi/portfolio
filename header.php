49647571cb408b9f783ef61ac3aaca9467d29cb7
Philip Zak <pz@myMBP.local>
2015-07-20 15:56:44 -0400

    added different css reset, removed mobile nav button from header, deleted wordpress-reset

--- header.php
+++ header.php
@@ -24,7 +24,6 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'portfolio' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
