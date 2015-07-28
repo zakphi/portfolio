6cffe56566119a6f8df52d3bc328418f92ed4352
Philip Zak <pz@myMBP.local>
2015-07-28 15:51:57 -0400

    added period to end of resume link

--- page-about.php
+++ page-about.php
@@ -8,7 +8,7 @@
       $linkedin_url = get_post_meta(get_the_ID(), "work_linkedin_url", true);
       $github_url = get_post_meta(get_the_ID(), "work_github_url", true);
     ?>
-    <span class="resume_link"><a href="<?php echo $resume_url; ?>">Click here for a printable version of resume</a></span>
+    <span class="resume_link"><a href="<?php echo $resume_url; ?>">Click here for a printable version of resume.</a></span>
     <div class="social_media">
       <a href="<?php echo $linkedin_url; ?>"><i class="fa fa-linkedin"></i></a>
       <a href="<?php echo $github_url; ?>"><i class="fa fa-github"></i></a>
