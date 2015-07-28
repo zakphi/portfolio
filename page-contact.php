e1537bc7194c7279218e538ac3b0ba1db564866c
Philip Zak <pz@myMBP.local>
2015-07-27 23:58:37 -0400

    Fixes #5, made contact form functioning, added formspree to process form

--- page-contact.php
+++ page-contact.php
@@ -1,11 +1,12 @@
-<form action="" method="">
-  <label>Name<span>*</span></label>
-  <input type="text" />
-  <label>Email<span>*</span></label>
-  <input type="email" />
-  <label>Phone</label>
+<form action="//formspree.io/philip@philipzak.com" method="POST">
+  <input type="hidden" name="_subject" value="Email from Site" />
+  <label for="name">Name<span>*</span></label>
+  <input type="text" id="name" name="name" />
+  <label for="email">Email<span>*</span></label>
+  <input type="email" id="email" name="_replyto" />
+  <label for="phone">Phone</label>
   <input type="phone" />
-  <label>Name<span>*</span></label>
-  <textarea></textarea>
-  <input type="submit" />
+  <label for="message">Name<span>*</span></label>
+  <textarea id="message" name="message"></textarea>
+  <input type="submit" id="submit" value="Send" />
 </form>
